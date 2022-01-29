b739d702b96ab0de507ddf7268fd792ee901592e
Betty Zhong <827941041@qq.com>
2022-01-29 11:58:44 -0500

    arm

--- TeamC/Assets/Scripts/PlayerMovement.cs
+++ TeamC/Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,20 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float downSpeed = 2f;
+    [SerializeField]
+    private GameObject leftHand;
+    [SerializeField]
+    private GameObject rightHand;
 
     private Camera camera;
     private bool isDown;
     Vector3 holder;
+    Vector3 holderRight;
+    Vector3 holderLeft;
     private float cameraPos;
     private float time;
+    private float leftH;
+    private float rightH;
 
     // Start is called before the first frame update
     void Start()
@@ -28,17 +36,30 @@ public class PlayerMovement : MonoBehaviour
             if(!isDown)
             {
                 cameraPos = camera.transform.position.y / 2;
-                holder = camera.transform.position;            
+                holder = camera.transform.position;
+                holderRight = rightHand.transform.position;
+                holderLeft = leftHand.transform.position;
+
+                leftH = leftHand.transform.position.y / 2;
+                rightH = rightHand.transform.position.y / 2;
             }
+
             isDown = true;
             time = time + Time.deltaTime * downSpeed;
             float result = Mathf.Lerp(holder.y, cameraPos, time);
+            float leftHandResult = Mathf.Lerp(holderLeft.y, leftH, time);
+            float rightHandResult = Mathf.Lerp(holderRight.y, rightH, time);
             camera.transform.position = new Vector3(camera.transform.position.x, result, camera.transform.position.z);
+            leftHand.transform.position = new Vector3(leftHand.transform.position.x, leftHandResult, leftHand.transform.position.z);
+            rightHand.transform.position = new Vector3(rightHand.transform.position.x, rightHandResult, rightHand.transform.position.z);
         }
         if (Input.GetKeyUp(KeyCode.LeftControl))
         {
             isDown = false;
             time = 0;
+            leftHand.transform.position = new Vector3(leftHand.transform.position.x, holderLeft.y, leftHand.transform.position.z);
+            rightHand.transform.position = new Vector3(rightHand.transform.position.x, holderRight.y, rightHand.transform.position.z);
+
         }
     }
 }
