db3bb4fcf13bd50e3e1a112b1a9ca650cc3f731a
TheMonkeo <98623702+TheMonkeo@users.noreply.github.com>
2022-01-28 23:02:15 -0500

    Moving Platform Script

---
+++ MovingPlatform.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Moving Platform Script
+public class MovingPlatform : MonoBehaviour
+{
+    //Loc1 and Loc2 cannot be children of the moving platform
+    [SerializeField]
+    private GameObject loc1;
+    [SerializeField]
+    private GameObject loc2;
+
+    [SerializeField]
+    private float moveSpeed = 2f;
+    private bool returning = false;
+
+    private void FixedUpdate()
+    {
+        if (returning)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, loc1.transform.position, Time.deltaTime * moveSpeed);
+            if(Vector3.Distance(transform.position, loc1.transform.position) < 0.001f)
+            {
+                returning = !returning;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, loc2.transform.position, Time.deltaTime * moveSpeed);
+            if (Vector3.Distance(transform.position, loc2.transform.position) < 0.001f)
+            {
+                returning = !returning;
+            }
+        }
+    }
+}
